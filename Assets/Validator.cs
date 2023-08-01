d4a1aef9f892c6f2312f9e91e7e7ed8ba5618d0f
shivam singh rathore <shivam.amgogames@gmail.com>
2023-08-01 20:50:35 +0530

    minor changes

--- Assets/Validator.cs
+++ Assets/Validator.cs
@@ -7,13 +7,14 @@ public class Validator : MonoBehaviour
     [SerializeField] List<knitRow> m_KnitRows;
     knitRow TopKnitRow=null;
     knitRow ActiveKnitRow;
-    List<knitRow> m_ActiveKnitRows;
+    List<knitRow> m_ActiveKnitRows = new List<knitRow>();
     
     void Start()
     {
         if (m_KnitRows.Count > 0)
         { 
             TopKnitRow = m_KnitRows[0]; 
+            ActiveKnitRow = m_ActiveKnitRows[m_ActiveKnitRows.Count-1];
         }
          
     }
@@ -28,6 +29,16 @@ public class Validator : MonoBehaviour
         if (m_ActiveKnitRows.Contains(KR))
             return;
         m_ActiveKnitRows.Add(KR);
+        ActiveKnitRow = m_ActiveKnitRows[m_ActiveKnitRows.Count - 1];
        
+    }
+    public void Validate(knitRow Topknitrow)
+    {
+        if (ActiveKnitRow.GetKnitData().KnitColor == ActiveKnitRow.GetKnitData().KnitColor)
+        {
+            //knit here
+            
+        }
+            
     }
 }
